328f0f3e7e5601d767db5a106cf21692488d908e
T-Loibl <taran@loibl.com>
2023-11-18 15:59:11 +0000

    Creates While True loop to check if letter valid and in word

---
+++ milestone_3.py
@@ -0,0 +1,19 @@
+import random
+
+word_list = ["cherry", "passion fruit", "pineapple", "banana", "kiwi"]
+word = random.choice(word_list)
+
+while True:
+    guess = input("Please enter a single letter.")
+
+    if guess.isalpha() and len(guess) == 1:
+        break 
+
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else: 
+    print(f"Sorry, {guess} is not in the word. Try again.")
+
